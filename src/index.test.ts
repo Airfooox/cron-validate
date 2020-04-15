899f125959a67e173ecf2c20956c7af8e937a2f9
Konstantin <konsti21@freenet.de>
2020-04-15 22:03:53 +0200

    test: add more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,5 @@
 import isCronValid from './index'
+import { getOptionPreset, registerOptionPreset } from './option'
 
 describe('Test cron validation', () => {
   it('Test cron length (5 chars allowed)', () => {
@@ -100,6 +101,12 @@ describe('Test cron validation', () => {
     expect(eightCharStringCron.isValid()).toBeFalsy()
   })
 
+  it('Test number input', () => {
+    expect(isCronValid('1,2,3 4,5,6 1 1 1').isValid()).toBeTruthy()
+
+    expect(isCronValid('01,02,03 04,05,06 01 01 01').isValid()).toBeTruthy()
+  })
+
   it('Test cron field assignment', () => {
     const cronResult = isCronValid('0 */4 * 1 6')
     expect(cronResult.isValid()).toBeTruthy()
@@ -177,6 +184,20 @@ describe('Test cron validation', () => {
     expect(
       isCronValid('5-7 2-4/2 1,2-4,5-8,10-20/3,20-30/4 * *').isValid()
     ).toBeTruthy()
+
+    expect(isCronValid('5-7,8-9,10-20,21-23 * * * *').isValid()).toBeTruthy()
+
+    expect(isCronValid('7-5 * * *').isValid()).toBeFalsy()
+
+    expect(
+      isCronValid('7-5 * * * *', { override: { useSeconds: true } }).isValid()
+    ).toBeFalsy()
+
+    expect(
+      isCronValid('* * * * 2020-2019', {
+        override: { useYears: true },
+      }).isValid()
+    ).toBeFalsy()
   })
 
   it('Test range limits', () => {
@@ -255,7 +276,146 @@ describe('Test cron validation', () => {
     ).toBeFalsy()
   })
 
-  it("Test preset system", () => {
+  it('Test preset system', () => {
+    registerOptionPreset('testPreset', {
+      presetId: 'testPreset',
+      useSeconds: true,
+      useYears: false,
+      seconds: {
+        minValue: 0,
+        maxValue: 59,
+      },
+      minutes: {
+        minValue: 0,
+        maxValue: 59,
+        lowerLimit: 10,
+        upperLimit: 30,
+      },
+      hours: {
+        minValue: 0,
+        maxValue: 23,
+      },
+      daysOfMonth: {
+        minValue: 0,
+        maxValue: 31,
+      },
+      months: {
+        minValue: 0,
+        maxValue: 12,
+      },
+      daysOfWeek: {
+        minValue: 0,
+        maxValue: 7,
+      },
+      years: {
+        minValue: 1970,
+        maxValue: 2099,
+      },
+    })
 
+    expect(getOptionPreset('testPreset')).toBeTruthy()
+
+    expect(
+      isCronValid('* * * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeFalsy()
+
+    expect(
+      isCronValid('* * * * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeTruthy()
+
+    expect(
+      isCronValid('* * * * * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeFalsy()
+
+    expect(
+      isCronValid('* * * * * * *', {
+        preset: 'testPreset',
+        override: {
+          useYears: true,
+        },
+      }).isValid()
+    ).toBeTruthy()
+
+    expect(
+      isCronValid('* 10-30 * * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeTruthy()
+
+    expect(
+      isCronValid('* 9 * * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeFalsy()
+
+    expect(
+      isCronValid('* 9 * * * *', {
+        preset: 'testPreset',
+        override: {
+          minutes: { lowerLimit: 9 },
+        },
+      }).isValid()
+    ).toBeTruthy()
+
+    expect(
+      isCronValid('* 10-30 */2 * * *', {
+        preset: 'testPreset',
+      }).isValid()
+    ).toBeTruthy()
+  })
+
+  it('Test invalid ranges', () => {
+    expect(
+      isCronValid('1-2-3 * * * * *', {
+        override: { useSeconds: true },
+      }).isValid()
+    ).toBeFalsy()
+
+    expect(isCronValid('* 1-2-3 * * * ').isValid()).toBeFalsy()
+
+    expect(isCronValid('* * 1-2-3 * * ').isValid()).toBeFalsy()
+
+    expect(isCronValid('1-* * * * *').isValid()).toBeFalsy()
+  })
+
+  it('Test invalid steps', () => {
+    expect(
+      isCronValid('1/2/3 * * * * *', {
+        override: { useSeconds: true },
+      }).isValid()
+    ).toBeFalsy()
+
+    expect(isCronValid('1/2/3 * * * *').isValid()).toBeFalsy()
+
+    expect(isCronValid('1/2/3/4 * * * *').isValid()).toBeFalsy()
+
+    expect(isCronValid('1/* * * * *').isValid()).toBeFalsy()
+
+    expect(isCronValid('1/0 * * * *').isValid()).toBeFalsy()
+  })
+
+  it('Test incomplete statements', () => {
+    expect(isCronValid('1/ * * * *').isValid()).toBeFalsy()
+
+    expect(isCronValid('20-30/ * * * * ').isValid()).toBeFalsy()
+
+    expect(isCronValid('*/ * * * * ').isValid()).toBeFalsy()
+  })
+
+  it('Test massive cron-expression', () => {
+    expect(
+      isCronValid(
+        '*/2,11,12,13-17,30-40/4 1,2,3,*/5,10-20 0-3,4-6,8-20/3,23 1,2,3,4,*/2,20-25/2,26-27 1-2,3-7/2,*/2,8-9/2 1,*/2,4-6',
+        {
+          override: { useSeconds: true },
+        }
+      ).isValid()
+    ).toBeTruthy()
   })
 })
